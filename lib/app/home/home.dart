418b21682a1768c59233ba48feda9674112b8fe5
1542021¸ <1542021@etu.cegep-ste-foy.qc.ca>
2019-02-04 10:13:42 -0500

    BottomNavigationBar

--- lib/app/home/home.dart
+++ lib/app/home/home.dart
@@ -2,7 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:tp3/app/learn.dart';
 import 'package:tp3/util/strings.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+class _HomeState extends State<Home>{
+  int _selectedIndex = 0;
+  final _widgetOptions = [
+    Learn(),
+    Text('IndexedStack 1:Entrainement'),
+  ];
   @override
   Widget build(BuildContext context) {
     var strings = Strings.of(context);
@@ -11,7 +22,24 @@ class Home extends StatelessWidget {
         appBar: AppBar(
           title: Text(strings.title),
         ),
-        body: Learn()
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.book), title:  Text('Apprendre')),
+          BottomNavigationBarItem(icon: Icon(Icons.create), title:  Text('Entrainement')),
+        ],
+          currentIndex: _selectedIndex,
+          onTap:  _onItemTapped,
+        ),
     );
   }
+
+  _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex= index;
+    });
+  }
 }
+
